d6b02b9bba019e05c9d0a6cfae37e0213f6bfc4e
DarshLakadia <126099628+DarshLakadia@users.noreply.github.com>
2023-03-14 14:00:41 +0530

    add association through middle table

--- association_query_interface/app/models/modifier.rb
+++ association_query_interface/app/models/modifier.rb
@@ -1,7 +1,7 @@
 class Modifier < ApplicationRecord
-  has_many :items
   has_one :rating, as: :ratingable
-    
+  has_many :item_modifiers
+  has_many :items, through: :item_modifiers
   default_scope { order("created_at desc")}
 
 end
